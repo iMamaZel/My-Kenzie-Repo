89c24576cdc5fed55e103623cb32f9f6f4e6da00
Jamie Goshay <jamie.goshay@mykenzie.snhu.edu>
2023-05-04 18:20:56 -0500

    Answered Assignment

---
+++ basicjs-variables-activity-iMamaZel.js
@@ -0,0 +1,95 @@
+// To run this assignment, right click on index.html in the Visual Studio file explorer to the left
+// and select "Open with Live Server"
+
+/***********************/
+/* Using Variables     */
+/***********************/
+
+// ----------------------------------------------------------------------------------------------
+console.log("Exercise One"); // Do not modify the lines like this before each exercise; these are to make the console output more readable :)
+// - Print the number 16 to the console.
+//
+// Write your code here 👇
+
+console.log(16);
+
+// ----------------------------------------------------------------------------------------------
+console.log("Exercise Two");
+// - Print the result of the expression six divided by three to the console.
+//
+// Write your code here 👇
+
+console.log(6/3);
+
+// ----------------------------------------------------------------------------------------------
+console.log("Exercise Three");
+// - Add parentheses to the expression below so that it prints 16 to the console (instead of 12)
+//
+// Write your code here 👇
+
+console.log((1 + 3) * 4);
+
+// ----------------------------------------------------------------------------------------------
+console.log("Exercise Four");
+// - Un-comment the following variable, and then on the next line, use console.log() to print it to the console.
+// Write your code here 👇
+
+let myFavoriteNumber = 6;
+console.log(myFavoriteNumber);
+// ----------------------------------------------------------------------------------------------
+console.log("Exercise Five");
+// - Create a variable called "otherNumber", assign the number 5 to it, and print it to the console on the next line.
+//
+// Write your code here 👇
+let otherNumber = 5;
+console.log(otherNumber);
+
+// ----------------------------------------------------------------------------------------------
+console.log("Exercise Six");
+// - Create a variable called "result", and do not assign it any value! Then log it to the console. (hint: this should print "undefined")
+//
+// Write your code here 👇
+let result;
+console.log(result)
+
+// ----------------------------------------------------------------------------------------------
+console.log("Exercise Seven");
+// - Now multiply "myFavoriteNumber" by "otherNumber" and assign that to "result".
+// - Then log it to the console
+// Hint: You can multiply with the * operator: For example, 3 * 5 would be 15
+//
+// Write your code here 👇
+ result = myFavoriteNumber * otherNumber
+console.log(result);
+
+// ----------------------------------------------------------------------------------------------
+console.log("Exercise Eight");
+// - Create two Variables, called "a" and "b". Assign the number 5 to "a" and the number 8 to "b".
+// - Then create a third variable called "difference". Subtract b from a, and assign the answer to the "difference" variable
+// Hint: subtraction uses the - operator
+// - Then print the difference to the console (This should print a negative number!).
+//
+// Write your code here 👇
+let a = 5
+let b = 8
+let difference = a - b
+console.log(difference);
+// When you are done, your console output should match the following: 
+/*
+Exercise One
+16
+Exercise Two
+2
+Exercise Three
+16
+Exercise Four
+6
+Exercise Five
+5
+Exercise Six
+undefined
+Exercise Seven
+30
+Exercise Eight
+-3
+*/
